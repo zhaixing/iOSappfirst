1fb89ea80a66d1b4e1abea21e60be2f44f4059b0
ultracpy <674917570@qq.com>
2016-03-06 15:30:21 +0800

    设置登录验证

---
+++ iOSDesign/iOSDesign/Classes/Module/Base/BaseTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BaseTableViewController.swift
+//  iOSDesign
+//
+//  Created by sam on 16/3/6.
+//  Copyright © 2016年 sam. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewController: UITableViewController {
+
+    //添加用户是否登录的标记
+    var userLogin = false
+    
+    
+    //loadview是苹果专门为 手写代码准备的，等于sb/xib
+    //一旦使用这个方法 sb/xib 就自动失效
+    //会自动检测view是否为空，如果为空，就自动调用loadView方法
+    
+    override func loadView() {
+        
+        userLogin ? super.loadView() : loadVisitorView()
+        /*三目预算符 如上一行
+        //自动去调用tableView
+        super.loadView()//真正进入的主页等页面，已经登录成功
+        
+        //下面的四行代码是调用自己定义的view
+        let v=UIView()
+        v.backgroundColor=UIColor.redColor()
+        view=v
+        print(view)
+        */
+    }
+    
+    private func loadVisitorView(){//红色验证的页面，需要用户去登录的引导界面
+        let v=UIView()
+        v.backgroundColor=UIColor.redColor()
+        view=v
+        print(view)
+    }
+    
+    //视图控制器 viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
--- iOSDesign/iOSDesign/Classes/Module/Main/MainViewController.swift
+++ iOSDesign/iOSDesign/Classes/Module/Main/MainViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MainViewController: UITabBarController {
 
+    @objc private func composeDidClick(){ //加上private会安全点，但是有个别的问题需要加上@objc。。兼容o-c机制了。。。
+        print( __FUNCTION__ )//打印方法名
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +27,9 @@ class MainViewController: UITabBarController {
         
         //测试语句，打印出类名，看程序是否执行到   classForCoder打印函数的类名
         print(tabBar.classForCoder)
+        
+        //设置点击事件
+        mainTabBar.composeBtn.addTarget(self, action: "composeDidClick", forControlEvents: .TouchUpInside)//相应事件composeDidClick很重要，习惯写在上面
     }
     
     private  func addChildViewControllers() {
@@ -51,8 +58,9 @@ class MainViewController: UITabBarController {
     //重载方法
     private func addChildViewController(vc:UIViewController,title:String,imageName:String){
         
-        //设置点击后字体的颜色
-        self.tabBar.tintColor = UIColor.init(red: 70/255.0, green: 200/255.0, blue: 202/255.0, alpha: 1.0)
+        //设置点击后字体的颜色 查找点
+//        self.tabBar.tintColor = UIColor.init(red: 70/255.0, green: 200/255.0, blue: 202/255.0, alpha: 1.0)
+        self.tabBar.tintColor = UIColor.orangeColor()
         let nav = UINavigationController(rootViewController: vc)
         vc.title=title
         vc.tabBarItem.image=UIImage(named: imageName)
